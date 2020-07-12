29b493a500284ea2708b304fb60c39fdddd6b60b
Hasan Usama <hasanusama.jalil@gmail.com>
2020-07-13 00:25:57 +0500

    Creating Advanced JS Assignment

---
+++ 1-String/index.js
@@ -0,0 +1,6 @@
+var str1=`My name is Hasan Usama
+I am 27 years old
+I work at PTCL`;
+
+console.log(str1);
+
---
+++ 2-Template-Literals/index.js
@@ -0,0 +1,18 @@
+var userName = "Muhammad Hasan Usama";
+var date= new Date();
+
+var coverLetter=`Concerned Authority, 
+GE.
+
+Dear Sir,
+
+I am interested in the Edison Engineering Development Program advertised on your website. Given my related experience and excellent capabilities, I would appreciate your consideration for this job opening.
+
+I take this opportunity to introduce myself, I am a graduate form NED University of Engineering and Technology. Currently, I am serving as Assistant Manager at PTCL in Wireless core department. I have also done 2-Month internship at P&G Port Qasim plant and a 6-week internship at the K-Electric Power Plant at KCCPP.
+
+I can assure you that with my academic background accompanied with the knowledge and skills will help me fit in your firm very well.
+                                                                                                                                         Yours sincerely,
+                                                                                                                                         ${userName}.
+                                                                                                                                         
+                                                                                                                                         ${date}`
+console.log(coverLetter);
